e7a9154f9f7fdf89dc9842fd5f4f7b90b4aee5d6
Rajnish <Rajnish@code.com>
2021-04-14 12:17:38 +0530

    add project

---
+++ Appcz/src/main/java/com/Appcz/App.java
@@ -0,0 +1,13 @@
+package com.Appcz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
